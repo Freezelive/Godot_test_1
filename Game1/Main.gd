1f01fcad90ff0b5ed5824a8f0d525cf43439f746
Freezelive <37311385+Freezelive@users.noreply.github.com>
2019-11-23 06:06:59 +0200

    Game1_update 1.1
    Changed to manage bodies using groups also capped framerate at 60FPS.
    Also added some comments

--- Game1/Main.gd
+++ Game1/Main.gd
@@ -13,30 +13,30 @@ func create_object_on_scene():
 	#-------------------------------------
 	#Declare the items for a sprite
 	#-------------------------------------
-	var sprite_texture = preload("res://Game1/resources/sprite.jpg")
+	var sprite_texture = preload("./resources/sprite.jpg")
 	var sprite_body = StaticBody2D.new()
 	var sprite_item=Sprite.new() 
 	
+	
 	#-------------------------------------
-	#merge them togheder
+	#merge sprite items  togheder
 	#-------------------------------------
 	sprite_item.texture = sprite_texture #add texture to sprite
 	sprite_item.scale=Vector2(0.5,0.5) #set sprite size
 	sprite_body.add_child(sprite_item) #add sprite to body
-	sprite_body.global_position=Vector2(0,0) #set location of body
-	#add_child(sprite_body) #add body to scene
 	#return the newly created sprite
 	return sprite_body
 var rng = RandomNumberGenerator.new()
 var bodyque = [] #hold elemnts creating an array
 var bodyque_lifetime=[]
 var screen_size=OS.get_window_safe_area().size
-var bodies_to_be_removed=[]
+
 func remove_object_to_scene(var body):
 	body.queue_free()
 	pass
 
 func add_object_to_scene(var body):
+	body.add_to_group("printed")  #adds body to group printed this also created the group printe if
 	add_child(body)
 	pass
 
@@ -45,15 +45,17 @@ func sprite_manage(var body_collection):
 	var current = 0
 	var tmp
 	while current < len(body_collection):
-		if body_collection[current].global_position == Vector2(0,0):
-			bodyque_lifetime.push_front(rng.randf_range (10, 20))
-			body_collection[current].scale=Vector2(randf(),randf()) #set sprite size
-			body_collection[current].global_position=Vector2(rng.randf_range (0, screen_size.x ),rng.randf_range (0, screen_size.y ))
+		#print_debug(get_tree().get_root())
+		print ( get_tree().get_nodes_in_group("printed").size())
+		if !body_collection[current].is_in_group("printed"): #check if body is in group printed
 			add_object_to_scene(body_collection[current])
-		bodyque_lifetime[current]=bodyque_lifetime[current]-2
+			bodyque_lifetime.push_front(rng.randf_range (10, 20)) #a variable to set lifetime of body
+			body_collection[current].scale=Vector2(randf(),randf()) #set sprite size
+			body_collection[current].global_position=Vector2(rng.randf_range (0, screen_size.x ),rng.randf_range (0, screen_size.y )) #set position
+		bodyque_lifetime[current]=bodyque_lifetime[current]-1 #used for body lifetime
 		
-		if bodyque_lifetime[current]<0:
-			tmp=body_collection[current]
+		if bodyque_lifetime[current]<0: #if body lifetime is smaller then 0 destroy it 
+			tmp=body_collection[current] #tmp var used to store body that will be destroyed
 			body_collection.remove(current)
 			bodyque_lifetime.remove(current)
 			remove_object_to_scene(tmp)
@@ -64,12 +66,13 @@ func sprite_manage(var body_collection):
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	#bodyque=insert.create_object_on_scene()
-	
+	Engine.target_fps=60
 
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	bodyque.push_front (create_object_on_scene()) #store nely created sprite
-	sprite_manage(bodyque)
+	sprite_manage(bodyque) #process newly created/existing bodys
+	
 	pass
