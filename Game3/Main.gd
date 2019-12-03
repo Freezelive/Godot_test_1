d37473846b2e0a96db3396dd87b95fb33456b0bb
Freezelive <37311385+Freezelive@users.noreply.github.com>
2019-12-03 10:35:23 +0200

    game 3 update
    code change still not proper functional

--- Game3/Main.gd
+++ Game3/Main.gd
@@ -7,6 +7,7 @@ var player_sprite=Sprite.new()
 var screen_size=OS.get_window_safe_area().size #get screen size
 var player_colision_shape = CollisionShape2D.new()
 var player_colision_form= RectangleShape2D.new()
+var player_area_2d=Area2D.new()
 
 #camera
 var player_follow_camera = Camera2D.new()
@@ -21,6 +22,10 @@ var rotation_speed = 1.5
 var velocity = Vector2()
 var target = Vector2()
 
+
+func alfatest(body):
+	print (body)
+	
 
 
 func object_spawn():
@@ -38,27 +43,47 @@ func object_spawn():
 	
 	var i =0
 	while i<8 :
-		var additional_object_body =  StaticBody2D.new()
-		var additional_object_sprite = Sprite.new() 
+#		var additional_object_body =  StaticBody2D.new()
+#		var additional_object_sprite = Sprite.new() 
+#		var additional_object_sprite_texture  = preload("./resources/sprite_object.jpg")
+#		var additional_object_colission = CollisionShape2D.new()
+#		var additional_object_colission_circleshape=CircleShape2D.new()
+#		var additional_object_area=Area2D.new()
+#		additional_object_sprite.scale=Vector2(0.1,0.1)
+#		additional_object_sprite.texture=additional_object_sprite_texture
+#		additional_object_body.add_child(additional_object_sprite)
+#		
+#		additional_object_colission_circleshape.radius=screen_size.x/2
+#		additional_object_colission.shape=additional_object_colission_circleshape
+#		
+#		additional_object_body.add_child(additional_object_area)
+#		additional_object_body.add_child(additional_object_colission)
+#		additional_object_body.add_to_group("list_of_objects")
+#		additional_object_body.global_position=(Vector2(global_position_of_object_x[i],global_position_of_object_y[i]))
+#		add_child(additional_object_body)
+#		objects_list.push_front(additional_object_body)
+		
+		var additional_object_sprite_main = Sprite.new() 
 		var additional_object_sprite_texture  = preload("./resources/sprite_object.jpg")
 		var additional_object_colission = CollisionShape2D.new()
 		var additional_object_colission_circleshape=CircleShape2D.new()
 		var additional_object_area=Area2D.new()
-		additional_object_area.connect("body_entered",self,"alfatest")
-		additional_object_sprite.scale=Vector2(0.1,0.1)
-		additional_object_sprite.texture=additional_object_sprite_texture
-		additional_object_body.add_child(additional_object_sprite)
 		
-		additional_object_colission_circleshape.radius=screen_size.x/2
+		additional_object_sprite_main.scale=Vector2(0.1,0.1)
+		additional_object_sprite_main.texture=additional_object_sprite_texture
+		
+		additional_object_colission_circleshape.radius=screen_size.x*5
 		additional_object_colission.shape=additional_object_colission_circleshape
 		
-		additional_object_body.add_child(additional_object_area)
-		additional_object_body.add_child(additional_object_colission)
-		additional_object_body.add_to_group("list_of_objects")
-		additional_object_body.global_position=(Vector2(global_position_of_object_x[i],global_position_of_object_y[i]))
-		add_child(additional_object_body)
-		objects_list.push_front(additional_object_body)
+		#additional_object_area.connect("body_entered",self,"alfatest",[self])
 		
+		additional_object_sprite_main.add_child(additional_object_area)
+		additional_object_sprite_main.add_child(additional_object_colission)
+		#additional_object_sprite_main.connect("body_entered",self,"_on_Area2D_body_entered")
+		additional_object_sprite_main.global_position=(Vector2(global_position_of_object_x[i],global_position_of_object_y[i]))
+		
+		add_child(additional_object_sprite_main)
+		objects_list.push_front(additional_object_sprite_main)
 		i +=1
 		
 			
@@ -67,9 +92,7 @@ func object_spawn():
 	
 	pass
 
-func alfatest():
-	print ("test")
-	
+
 
 
 # Called when the node enters the scene tree for the first time.
@@ -84,6 +107,10 @@ func _ready():
 	player_kin_body.add_child(player_colision_shape)
 	player_kin_body.position=Vector2(100,100)
 	
+	player_area_2d.connect("body_entered",self,"_on_Area2D_body_entered")
+	#player_area_2d.connect("body_entered",self,"_on_Area2D_body_entered")
+	player_kin_body.add_child(player_area_2d)
+	print(player_area_2d.get_property_list().find("connect"))
 	#camera
 	player_follow_camera.make_current()  #make this camera current
 	player_follow_camera.name=active_player_follow_camera #set name to be used later
@@ -91,11 +118,11 @@ func _ready():
 	
 	add_child(player_kin_body) #add body to main scene
 	for i in player_kin_body.get_child_count():  #this for iterates all the children in the $player_kin_body and checks the name set above and save varaible for later use
-		print (player_kin_body.get_child(i).name)
-		print (active_player_follow_camera)
+	#	print (player_kin_body.get_child(i).name)
+	#	print (active_player_follow_camera)
 		if player_kin_body.get_child(i).name == active_player_follow_camera:
 			active_player_follow_camera=player_kin_body.get_child(i)
-			print (active_player_follow_camera)
+	#		print (active_player_follow_camera)
 		
 	
 	
@@ -107,8 +134,6 @@ func _ready():
 	
 	pass # Replace with function body.
 
-func _enter_tree():
-	print ("yes")
 
 func grid_move():
 	velocity = Vector2()
@@ -149,11 +174,17 @@ func _process(delta):
 	
 	
 	grid_move()
-	
 	#player_kin_body.move_and_slide(velocity)
 	#var tmp = player_kin_body.move_and_collide(velocity*delta)
 	var tmp=player_kin_body.move_and_collide(velocity*delta)
 	#print (tmp, " ", player_kin_body)
-	if tmp != null:
-		layer_and_mask_manager(tmp,true)
-	pass
+	#if tmp != null:
+	#	layer_and_mask_manager(tmp,true)
+	#pass
+
+
+
+func _on_Area2D_body_entered(body):
+	print (body)
+	print ("2")
+	pass # Replace with function body.
